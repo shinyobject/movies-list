d8dee831bb4bea540c4b91666fe7be8b15913e18
shinyobject <peter@shinyobject.com>
2024-08-03 11:13:43 -0700

    add youtube url scheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,45 @@
+import React from 'react';
+
 import './App.css';
 import { movies } from './movies.ts';
 
+
+
+function YouTubeLink({ isIOS,videoId }: { isIOS: boolean, videoId: string}) {
+ 
+  const link = `results?search_query=${videoId.replace(' ','+')}`
+
+  const openYouTubeLink = () => {
+    if (isIOS) {
+      window.location.href = `youtube://www.youtube.com/${link}`;
+      setTimeout(() => {
+        window.location.href = `https://www.youtube.com/${link}`;
+      }, 500);
+    } else {
+      window.location.href = `https://www.youtube.com/${link}`;
+    }
+  };
+
+  return (
+    <a href="#" onClick={openYouTubeLink}>{videoId}</a>
+  );
+}
+
 function App() {
+  const isIOS = () => {
+    const userAgent = window.navigator.userAgent.toLowerCase();
+    return /iphone|ipad|ipod/.test(userAgent);
+  };
+  const device = isIOS();
+
   return (
     <>
       <h1>Movies on YouTube</h1>
       <ul>
         {movies.map((movie) => (
           <li key={movie}>
-            <a
-              href={`https://youtube.com/results?search_query=${movie.replace(
-                ' ',
-                '+'
-              )}`}
-            >
-              {movie}
-            </a>
+            <YouTubeLink isIOS={device} videoId={movie} />
+          
           </li>
         ))}
       </ul>
